83c20b943c1731fbcd23163a695f120760c33e87
deBeee <fiacik12345@gmail.com>
2024-02-09 23:27:43 +0100

    adjusted unit test methods

--- src/test/java/com/junioroffers/domain/offer/InMemoryOfferRepositoryTestImpl.java
+++ src/test/java/com/junioroffers/domain/offer/InMemoryOfferRepositoryTestImpl.java
@@ -1,16 +1,67 @@
 package com.junioroffers.domain.offer;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.stream.StreamSupport;
 
 public class InMemoryOfferRepositoryTestImpl implements OfferRepository{
     Map<String, Offer> offerDatabase = new ConcurrentHashMap<>();
+
+    @Override
+    public <S extends Offer> List<S> saveAll(Iterable<S> entities) {
+        return StreamSupport.stream(entities.spliterator(), false)
+                .map(this::save)
+                .toList();
+    }
+
     @Override
     public List<Offer> findAll() {
         return this.offerDatabase.values().stream().toList();
+    }
+
+    @Override
+    public List<Offer> findAllById(Iterable<String> strings) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(String s) {
+
+    }
+
+    @Override
+    public void delete(Offer entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends String> strings) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Offer> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
     }
 
     @Override
@@ -19,7 +70,12 @@ public class InMemoryOfferRepositoryTestImpl implements OfferRepository{
     }
 
     @Override
-    public Offer save(Offer entity) {
+    public boolean existsById(String s) {
+        return false;
+    }
+
+    @Override
+    public <S extends Offer> S save(S entity) {
         if (offerDatabase.values().stream().anyMatch(offer -> offer.url().equals(entity.url()))) {
             throw new OfferDuplicateException(entity.url());
         }
@@ -31,17 +87,71 @@ public class InMemoryOfferRepositoryTestImpl implements OfferRepository{
                 entity.url()
         );
         this.offerDatabase.put(offerToSave.id(), offerToSave);
-        return offerToSave;
+        return (S) offerToSave;
     }
 
-    @Override
     public List<Offer> saveAll(List<Offer> offersToSave) {
         offersToSave.forEach(this::save);
         return offersToSave;
     }
 
     @Override
-    public boolean existsByOfferUrl(String url) {
+    public boolean existsByUrl(String url) {
         return this.offerDatabase.values().stream().anyMatch(offer -> offer.url().equals(url));
+    }
+
+    @Override
+    public <S extends Offer> S insert(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Offer> List<S> insert(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public <S extends Offer> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends Offer> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends Offer> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends Offer> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Offer> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends Offer> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends Offer, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
+    }
+
+    @Override
+    public List<Offer> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<Offer> findAll(Pageable pageable) {
+        return null;
     }
 }
