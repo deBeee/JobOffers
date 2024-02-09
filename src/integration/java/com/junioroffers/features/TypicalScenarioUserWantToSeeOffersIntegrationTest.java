1b49a67679bc1f7f21d5b0514a53be1503d9dba6
deBeee <fiacik12345@gmail.com>
2024-02-09 23:41:30 +0100

    integration testing

--- src/integration/java/com/junioroffers/features/TypicalScenarioUserWantToSeeOffersIntegrationTest.java
+++ src/integration/java/com/junioroffers/features/TypicalScenarioUserWantToSeeOffersIntegrationTest.java
@@ -3,28 +3,37 @@ package com.junioroffers.features;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.junioroffers.BaseIntegrationTest;
 import com.junioroffers.SampleJobOfferResponse;
+import com.junioroffers.domain.offer.OfferRepository;
 import com.junioroffers.infrastructure.offer.scheduler.HttpOffersScheduler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TypicalScenarioUserWantToSeeOffersIntegrationTest extends BaseIntegrationTest implements SampleJobOfferResponse {
 
     @Autowired
     HttpOffersScheduler httpOffersScheduler;
 
+    @Autowired
+    OfferRepository offerRepository;
+
     @Test
     public void user_want_to_see_offers_but_have_to_be_logged_in_and_external_server_should_have_some_offers() {
         //step 1: there are no offers in external HTTP server
-
+        //given & when & then
         wireMockServer.stubFor(WireMock.get("/offers")
                 .willReturn(WireMock.aResponse()
                         .withStatus(HttpStatus.OK.value())
                         .withHeader("Content-Type", "application/json")
-                        .withBody(bodyWithFourOffersJson())));
+                        .withBody(bodyWithZeroOffersJson())));
 
         //step 2: scheduler ran 1st time and made GET to external server and system added 0 offers to database
-        httpOffersScheduler.fetchAllOffersAndSaveAllIfNotExists(); //explicit execution of scheduler (so mocked server can get up before http client sends request) and respond to that request
+        //given & when
+        httpOffersScheduler.fetchAllOffersAndSaveAllIfNotExists(); //explicit execution of scheduler (so mocked server can get up before http client sends request) and respond to that request, scheduler is tested separately
+        //then
+        assertThat(offerRepository.findAll()).isEmpty();
 
         //step 3: user tried to get JWT token by requesting POST /token with username=someUser, password=somePassword and system returned UNAUTHORIZED(401)
         //step 4: user made GET /offers with no jwt token and system returned UNAUTHORIZED(401)
