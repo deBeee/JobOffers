074816e200b5db1aa7de7e2d2330d455cca47680
deBeee <fiacik12345@gmail.com>
2024-02-19 22:42:35 +0100

    added endpoint and related assertion in integration test

--- src/main/java/com/junioroffers/infrastructure/offer/controller/OfferRestController.java
+++ src/main/java/com/junioroffers/infrastructure/offer/controller/OfferRestController.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,12 +15,19 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @Log4j2
-public class OfferController {
+@RequestMapping("/offers")
+public class OfferRestController {
 
     private final OfferFacade offerFacade;
-    @GetMapping(path = "/offers")
+    @GetMapping
     public ResponseEntity<AllOffersResponseDto> getOffers(){
         List<OfferDto> allOffers = this.offerFacade.findAllOffers();
         return ResponseEntity.ok(new AllOffersResponseDto(allOffers));
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<OfferDto> getOfferById(@PathVariable String id){
+        OfferDto offer = this.offerFacade.findOfferById(id);
+        return ResponseEntity.ok(offer);
     }
 }
