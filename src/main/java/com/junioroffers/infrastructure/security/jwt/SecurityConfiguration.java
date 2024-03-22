3208991e6562543f1a99dd53d556720539e8b9dd
deBeee <fiacik12345@gmail.com>
2024-03-22 16:43:33 +0100

    refactoring security configuration allowing browser sending preflight requests

--- src/main/java/com/junioroffers/infrastructure/security/jwt/SecurityConfiguration.java
+++ src/main/java/com/junioroffers/infrastructure/security/jwt/SecurityConfiguration.java
@@ -7,15 +7,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @AllArgsConstructor
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     private final JwtAuthTokenFilter jwtAuthTokenFilter;
@@ -36,7 +41,19 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors().configurationSource(corsConfigurationSource());
         httpSecurity.csrf().disable();
         httpSecurity.authorizeRequests()
                 .antMatchers("/swagger-ui/**").permitAll()
